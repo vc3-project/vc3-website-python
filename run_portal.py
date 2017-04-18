03045c2e72a4d551ba895ea58c0de4739261da4a
Jaro <jeremyvan614@gmail.com>
2017-04-18 16:22:36 -0500

    Update script to run CherryPy Web Server

--- run_portal.py
+++ run_portal.py
@@ -2,5 +2,22 @@
 
 from portal import app
 
+# Import CherryPy
+import cherrypy
+
 if __name__ == '__main__':
+    # Mount the application
+    cherrypy.tree.graft(app, "/")
+
+    # Unsubscribe the default server
+    cherrypy.server.unsubscribe()
+
+    # Instantiate a new server object
+    server = cherrypy._cpserver.Server()
+
+    # Configure the server object
+    server.socket_host = "0.0.0.0"
+    server.socket_port = 8080
+    server.thread_pool = 30
+
     app.run(host='localhost', ssl_context=('./ssl/server.crt', './ssl/server.key'))
