e1140dd71717a71ba4e5da4dcb6fd0aea9e606f9
Jaro <jeremyvan614@gmail.com>
2017-04-19 14:50:29 -0500

    Update CherryPy workaround by Such

--- run_portal.py
+++ run_portal.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 
+import sys
+
 from portal import app
 
 # Import CherryPy
 import cherrypy
+from cherrypy import wsgiserver
 
 if __name__ == '__main__':
+    # workaround for now
+    d = wsgiserver.WSGIPathInfoDispatcher({'/': app})
+    server = wsgiserver.CherryPyWSGIServer(('www-dev.virtualclusters.org', 8080), d)
+    try:
+        server.start()
+    except KeyboardInterrupt:
+        server.stop()
+    sys.exit(0)
+
     # Mount the application
     cherrypy.tree.graft(app, "/")
 
@@ -16,13 +28,22 @@ if __name__ == '__main__':
     server = cherrypy._cpserver.Server()
 
     # Configure the server object
-    server.socket_host = "192.170.227.145"
+    # server.socket_host = "192.170.227.145"
+    server.socket_host = "www-dev.virtualclusters.org"
     server.socket_port = 8080
     server.thread_pool = 30
 
     # Subscribe this server
     server.subscribe()
 
+    # Enable debugging
+    cherrypy.config.update({
+        'engine.autoreload_on': True,
+        'log.screen': True,
+        'server.socket_port': 8080,
+        'server.socket_host': "www-dev.virtualclusters.org"
+    })
+    app.debug = True
     # Start the server engine (Option 1 *and* 2)
     cherrypy.engine.start()
     cherrypy.engine.block()
