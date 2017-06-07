6a647683c6cc005aef5c4a2249c6a2e17087ac98
Jaro <jeremyvan614@gmail.com>
2017-06-07 15:12:24 -0500

    Debugging Autoversioning filter

--- portal/views.py
+++ portal/views.py
@@ -513,13 +513,13 @@ def projectpages():
 # Autoversioning Filter
 # -----------------------------------------
 
-@app.template_filter('autoversion')
-def autoversion_filter(filename):
-    # determining fullpath might be project specific
-    fullpath = os.path.join('portal/', filename[1:])
-    try:
-        timestamp = str(os.path.getmtime(fullpath))
-    except OSError:
-        return filename
-    newfilename = "{0}?v={1}".format(filename, timestamp)
-    return newfilename
+# @app.template_filter('autoversion')
+# def autoversion_filter(filename):
+#     # determining fullpath might be project specific
+#     fullpath = os.path.join('portal/', filename[1:])
+#     try:
+#         timestamp = str(os.path.getmtime(fullpath))
+#     except OSError:
+#         return filename
+#     newfilename = "{0}?v={1}".format(filename, timestamp)
+#     return newfilename
