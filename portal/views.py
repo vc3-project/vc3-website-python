aad12d5a61a1d6ff197f5d441e11d0bf0128b5c2
Jaro <jeremyvan614@gmail.com>
2017-06-07 15:07:16 -0500

    Added autoversioning to filter css and js caching

--- portal/views.py
+++ portal/views.py
@@ -2,6 +2,8 @@ from flask import (abort, flash, redirect, render_template, request,
                    session, url_for)
 import requests
 
+import os
+
 try:
     from urllib.parse import urlencode
 except:
@@ -505,3 +507,19 @@ def project():
 @authenticated
 def projectpages():
     return render_template('projects_pages.jinja2')
+
+
+# -----------------------------------------
+# Autoversioning Filter
+# -----------------------------------------
+
+@app.template_filter('autoversion')
+def autoversion_filter(filename):
+    # determining fullpath might be project specific
+    fullpath = os.path.join('portal/', filename[1:])
+    try:
+        timestamp = str(os.path.getmtime(fullpath))
+    except OSError:
+        return filename
+    newfilename = "{0}?v={1}".format(filename, timestamp)
+    return newfilename
