0f17a8cfa82badf8c57149b58de9aa6e551f482b
root <root@www-dev.virtualclusters.org>
2017-06-27 14:19:59 -0500

    Community page and route update run_python

--- run_portal.py
+++ run_portal.py
@@ -35,16 +35,6 @@ if __name__ == "__main__":
     # Instantiate a new server object
     server = cherrypy._cpserver.Server()
 
-
-    # Mount the application
-    cherrypy.tree.graft(app, "/")
-   
-    # Unsubscribe the default server
-    cherrypy.server.unsubscribe()
-   
-    # Instantiate a new server object
-    server = cherrypy._cpserver.Server()
-   
     # Configure the server object
     server.socket_host = "www-dev.virtualclusters.org"
     server.socket_port = 8080
@@ -68,3 +58,4 @@ if __name__ == "__main__":
     #     freezer.freeze()
     # else:
     #     app.run(host='localhost', ssl_context=('./ssl/server.crt', './ssl/server.key'))
+
