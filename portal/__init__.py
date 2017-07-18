c880001fd37254dfd74800b944f71545d75f1e2b
Jaro <jeremyvan614@gmail.com>
2017-07-18 12:01:18 -0500

    Removed submodule vc3-info-service and update requirements

--- portal/__init__.py
+++ portal/__init__.py
@@ -2,25 +2,25 @@ from flask import Flask
 import json
 from flask_flatpages import FlatPages
 from flask_frozen import Freezer
-
+# from vc3client.client import VC3ClientAPI
 from portal.database import Database
 
-# from configparser import ConfigParser
-# from vc3_client.vc3client.client import VC3ClientAPI
 
 __author__ = 'Jeremy Van <jeremyvan@uchicago.edu>'
 
 
 app = Flask(__name__)
 app.config.from_pyfile('portal.conf')
+# app.config.from_pyfile('/vc3-client/etc/vc3-client.conf')
 pages = FlatPages(app)
 freezer = Freezer(app)
 
 database = Database(app)
 
 # cp = ConfigParser()
-# cp.read('/vc3_client/etc/vc3-client.conf')
-# vc3clientobj = VC3ClientAPI(cp)
+# cp.read('/vc3-client/etc/vc3-client.conf')
+# vc3clientobj = VC3ClientAPI(app)
+
 
 with open(app.config['DATASETS']) as f:
     datasets = json.load(f)
