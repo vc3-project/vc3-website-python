9efa84cf90b7fdbd70a55121f64593b5216da279
Jaro <jeremyvan614@gmail.com>
2017-11-07 09:35:56 -0600

    Tested email restriction works and added comment

--- portal/views.py
+++ portal/views.py
@@ -268,8 +268,10 @@ def authcallback():
         profile = None
 
         email = id_token.get('email', '')
-
-        if not (email.split("@")[-1].split(".")[-1] in ["edu", "gov"]):
+        # Restrict Email access to only .edu, .gov, and .org
+        # User must have a valid institutional affiliation
+        # Otherwise return to error page, explaining restricted access
+        if not (email.split("@")[-1].split(".")[-1] in ["edu", "gov", "org"]):
             return render_template('email_error.html')
 
         for user in userlist:
