49740d57fb276f340b9ddd929f8574583a1e52a1
Jaro <jeremyvan614@gmail.com>
2018-03-01 13:09:59 -0600

    Update allocation states and klannon whitelist

--- portal/views.py
+++ portal/views.py
@@ -39,7 +39,8 @@ whitelist_email = ['jeremyvan@uchicago.edu', 'briedel@uchicago.edu',
                    'rwg@uchicago.edu', 'sthapa@ci.uchicago.edu',
                    'tshaffe1@nd.edu', 'czheng2@nd.edu', 'pivie@nd.edu',
                    'jlstephen@uchicago.edu', 'balamurugan@uchicago.edu',
-                   'gfarr@uchicago.edu', 'ddl@illinois.edu', 'ivukotic@uchicago.edu']
+                   'gfarr@uchicago.edu', 'ddl@illinois.edu',
+                   'ivukotic@uchicago.edu', 'klannon@nd.edu']
 
 # Create a custom error handler for Exceptions
 
