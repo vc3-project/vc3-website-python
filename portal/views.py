7f0908557c96dcc83eff81d4ed1a8dbaf1c6fee9
Jaro <jeremyvan614@gmail.com>
2018-03-07 01:27:19 -0600

    Fix breadcrumb portal home, full instructions on allocations and vc pages

--- portal/views.py
+++ portal/views.py
@@ -1070,7 +1070,7 @@ def validate_allocation(name):
         allocation.action = "validate"
 
     vc3_client.storeAllocation(allocation)
-    flash('Allocation is validating', 'warning')
+    # flash('Allocation is validating', 'warning')
     return redirect(url_for('view_allocation', name=name))
 
 
