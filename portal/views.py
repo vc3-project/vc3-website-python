013fd9d65d50a8b50fd77095db29404694c5dcaf
Jaro <jeremyvan614@gmail.com>
2018-06-14 15:08:57 -0500

    Temp rm whitelist from prod

--- portal/views.py
+++ portal/views.py
@@ -48,7 +48,10 @@ whitelist_email = ['jeremyvan@uchicago.edu', 'briedel@uchicago.edu',
                    'gfarr@uchicago.edu', 'ddl@illinois.edu',
                    'ivukotic@uchicago.edu', 'ivukotic@cern.ch',
                    'klannon@nd.edu', 'rob.rwg@gmail.com',
-                   'rwg@hep.uchicago.edu', 'robert.w.gardner@cern.ch']
+                   'rwg@hep.uchicago.edu', 'robert.w.gardner@cern.ch',
+                   'jzhou93@uchicago.edu', 'cnweaver@uchicago.edu',
+                   'annawoodard@uchicago.edu', 'awoodard@nd.edu',
+                   'yiyangou@uchicago.edu']
 
 # Create a custom error handler for Exceptions
 
@@ -222,10 +225,11 @@ def show_profile_page():
                 session['ssh'] = sshpubstring
         else:
             if session['email'] not in whitelist_email:
-                return redirect(url_for('whitelist_error'))
-            else:
-                flash('Please complete any missing profile fields before '
-                      'launching a cluster.', 'warning')
+                # return redirect(url_for('whitelist_error'))
+                pass
+            # else:
+            #     flash('Please complete any missing profile fields before '
+            #           'launching a cluster.', 'warning')
 
         if request.args.get('next'):
             session['next'] = get_safe_redirect()
@@ -406,7 +410,8 @@ def authcallback():
             return redirect(url_for('show_profile_page',
                                     next=url_for('show_profile_page')))
         if session['email'] not in whitelist_email:
-            return redirect(url_for('whitelist_error'))
+            # return redirect(url_for('whitelist_error'))
+            pass
 
         return redirect(url_for('portal'))
 
