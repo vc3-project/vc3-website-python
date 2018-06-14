424db00be87fd6aef8beebc856fe0dd946f6c541
Jaro <jeremyvan614@gmail.com>
2018-06-14 14:25:02 -0500

    Updated whitelist email and remove jupyter for prod

--- portal/views.py
+++ portal/views.py
@@ -50,7 +50,8 @@ whitelist_email = ['jeremyvan@uchicago.edu', 'briedel@uchicago.edu',
                    'klannon@nd.edu', 'rob.rwg@gmail.com',
                    'rwg@hep.uchicago.edu', 'robert.w.gardner@cern.ch',
                    'jzhou93@uchicago.edu', 'cnweaver@uchicago.edu',
-                   'annawoodard@uchicago.edu', 'yiyangou@uchicago.edu']
+                   'annawoodard@uchicago.edu', 'awoodard@nd.edu',
+                   'yiyangou@uchicago.edu']
 
 # Create a custom error handler for Exceptions
 
