2a317466fc59ef7e8589333e77de4b36f079226b
Jaro <jeremyvan614@gmail.com>
2018-06-26 14:23:18 -0500

    Add Kevin Pedro to whitelist vc3-prod

--- portal/views.py
+++ portal/views.py
@@ -51,7 +51,8 @@ whitelist_email = ['jeremyvan@uchicago.edu', 'briedel@uchicago.edu',
                    'rwg@hep.uchicago.edu', 'robert.w.gardner@cern.ch',
                    'jzhou93@uchicago.edu', 'cnweaver@uchicago.edu',
                    'annawoodard@uchicago.edu', 'awoodard@nd.edu',
-                   'anna.elizabeth.woodard@cern.ch', 'yiyangou@uchicago.edu']
+                   'anna.elizabeth.woodard@cern.ch', 'yiyangou@uchicago.edu',
+                   'kevin.jerome.pedro@cern.ch']
 
 # Create a custom error handler for Exceptions
 
