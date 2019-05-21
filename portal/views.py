47c02507968b5c343018d94aa13e0285d5b23650
Jaro <jeremyvan614@gmail.com>
2019-05-21 12:43:38 -0500

    add cody to dev

--- portal/views.py
+++ portal/views.py
@@ -54,8 +54,8 @@ whitelist_email = ['jeremyvan@uchicago.edu', 'briedel@uchicago.edu',
                    'annawoodard@uchicago.edu', 'awoodard@nd.edu',
                    'anna.elizabeth.woodard@cern.ch', 'yiyangou@uchicago.edu',
                    'kevin.jerome.pedro@cern.ch', 'ndev@nd.edu',
-                   'manglano@uchicago.edu', 'chard@uchicago.edu',
-                   'djordan66@uchicago.edu']
+                   'ckankel@nd.edu', 'manglano@uchicago.edu',
+                   'chard@uchicago.edu', 'djordan66@uchicago.edu']
 
 # Create a custom error handler for Exceptions
 
