c4e115fcc4d07da987c5e74379b5548d6292cf38
Jeremy Van <jeremyvan614@gmail.com>
2020-08-03 15:52:06 -0500

    add diego and tibor to whitelist prod and update markdown version requirements

--- portal/views.py
+++ portal/views.py
@@ -59,7 +59,7 @@ whitelist_email = ['jeremyvan@uchicago.edu', 'briedel@uchicago.edu',
                    'swallen2@nd.edu', 'Samuel.O.WallenRussell.1@nd.edu',
                    'paschos@uchicago.edu', 'mweinberg@uchicago.edu',
                    'marc.gabriel.weinberg@cern.ch', 'lagray@fnal.gov',
-                   'lindsey.gray@cern.ch']
+                   'lindsey.gray@cern.ch', 'tibor.simko@cern.ch', 'diego.rodriguez@cern.ch']
 
 # Create a custom error handler for Exceptions
 
